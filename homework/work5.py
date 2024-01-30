c76b1215ae4c2df2f612ab58947aaa6cad7734c5
Dehong <askarovbob.com>
2024-01-30 14:38:20 +0600

    Domashka

---
+++ homework/work5.py
@@ -0,0 +1,35 @@
+
+a = 10 // 5
+b = 10 / 5
+
+if a == b:
+    print("ravni")
+if a == b:
+    print(a, b)
+else:
+    print("ne ravni")
+
+a = -2
+
+if a <= 0:
+    print(a)
+
+a = 10
+b = 5
+if a > 0 and b > 0:
+    print(a,b)
+
+a = 10
+b = 5
+if a > b:
+    print(a + 2)
+else:
+    print(b + 2)
+
+
+a = 45
+b = 6
+if a / b:
+    print(a / b)
+else:
+    print("ne delitsya")
