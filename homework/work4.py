017e0ee45302efc1aa8789c3c95b7f55f8e59d59
Dehong <askarovbob.com>
2024-01-30 14:31:23 +0600

    Domashka

---
+++ homework/work4.py
@@ -0,0 +1,38 @@
+a = 2 ** 3
+b = 3 ** 2
+if a > b:
+    print("a>b")
+
+else:
+    print("3**2 bolshe")
+
+a = 50
+if 0  <= a <= 21 or 57 <= a <= 100:
+    print("mojno")
+
+else:
+    print("nelzya")
+
+a = 4
+if a % 2 == 0:
+    print("chetniy")
+elif a % 2 != 0:
+    print("ne chetniy")
+if a // 3 == 0:
+    print("delitsya na tri bez ostatka")
+elif a // 3 != 0:
+    print("ne delitsya")
+if a * a > 1000:
+    print("bolshe 1000")
+elif a * a < 1000:
+    print("menshe 1000")
+
+a = 10
+
+if a > 10:
+    print("bolshe 10")
+
+else:
+   print("menshe 10")
+
+
