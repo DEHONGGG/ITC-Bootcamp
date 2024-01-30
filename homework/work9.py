d9c9b22434361c3184bf320e771505e869f2247c
Dehong <askarovbob.com>
2024-01-30 15:19:04 +0600

    Domashka

---
+++ homework/work9.py
@@ -0,0 +1,15 @@
+a = (1, 2, 3, 4, 5)
+print(a)
+
+names = ("Nikita", "Stas", "Gena")
+print(names[0])
+print(names[1])
+print(names[2])
+
+list = [1, 2.1, "tri", (7, 2, 3), False]
+print(list)
+
+list = ("Nikita" , "Stas" , "Gena" , "Turba" , "Dyusha")
+a = " ".join(list)
+print(a)
+
