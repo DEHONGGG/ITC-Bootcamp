3e997307c6fef444089430d692716dbeeb383a45
Dehong <askarovbob.com>
2024-01-30 13:45:19 +0600

    Domashka

---
+++ homework/work.py
@@ -0,0 +1,46 @@
+
+a = (17*3)
+b = (12*5)
+c = (12**3)
+d = (13*7)
+e = (4**5)
+f = (512+512)
+print(a < b)
+print(c > d)
+print(e == f)
+
+
+
+a = 17925
+print((34**2) < a)
+print((26*3) < a)
+print((17*33) <a)
+print((4394*4) <a)
+
+
+
+a = 10
+b = 7
+c = a + b
+d = 3
+e = c * d
+f = e ** 2
+print(f - 193432)
+
+
+a = 4.8
+b = 3
+c = 7
+
+d = (7 % 3) * 4.8
+
+print(d)
+
+f = c + b
+e = 10
+
+print(f == e)
+print(b < e)
+
+
+
