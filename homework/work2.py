14bc1af2ca6c51423519ac472eef52f2168bee79
Dehong <askarovbob.com>
2024-01-30 14:17:40 +0600

    Domashka

---
+++ homework/work2.py
@@ -0,0 +1,20 @@
+a = -21//10
+print(a)
+#потому что деление по модулю
+
+god_dr = 1997
+etot_god = 2024
+
+print((etot_god - god_dr) - 2)
+print((etot_god - god_dr) + 2)
+
+a = 27
+b = 100
+print((a/b)*100)
+
+a = 25
+b = 75
+c = 10
+d = 95
+print((a + b + c +d) / 4)
+
