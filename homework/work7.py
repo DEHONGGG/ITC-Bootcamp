d0007af33411a94459f352410238fb5c3399139e
Dehong <askarovbob.com>
2024-01-30 14:55:02 +0600

    Domashka

---
+++ homework/work7.py
@@ -0,0 +1,32 @@
+
+a = 23
+b = 33
+c = 55
+if a > b and a > c:
+    print(a)
+elif b > a and b > c:
+    print(b)
+else:
+    print(c)
+if a < b and a < c:
+    print(a)
+elif b < a and b < c:
+    print(b)
+else:
+    print(c)
+
+a = 17391
+b = 546
+c = 934
+d = 17
+if (a % d) < (b % d) and (a % d) < (c % d):
+   print("a % 17 ostatko menshe")
+elif (b % d) < (a % d) and (b % d) < (c % d):
+    print("b % 17 ostatko menshe")
+else:
+    print("c % 17 ostatka menshe")
+
+x = 13
+a = 172
+if (x ** 2) <= a:
+    print(x**4)
