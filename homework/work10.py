03cd989dba32c257157016bf5e2f4ab644af2470
Dehong <askarovbob.com>
2024-01-30 15:23:08 +0600

    Domashka

---
+++ homework/work10.py
@@ -0,0 +1,18 @@
+a = [1, 2, 3, 4, 5]
+b = [6, 7, 8, 9, 10]
+
+a.extend(b)
+print(a)
+
+names = ["Oskar", 'Jack', 'Jimmy', 'Jackson', 'Jhon', 'Jackson', 'Jhon',  'Jimmy', 'Jackson', 'Jhon','Jack', 'Jimmy', 'Jack', 'Jackson', 'Jhon', 'Jackson', 'Jhon','Jack', 'Jimmy', 'Jack', 'Jackson', 'Jhon',]
+a = names.count("Jack")
+print(a)
+names.remove("Oskar")
+print(names)
+
+list = []
+list.append("Toksh")
+list.append("1997")
+list.append("python")
+print(list)
+
