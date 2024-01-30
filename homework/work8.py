29a4767ef416ef97e7ba31a86c3b6e100dd93a69
Dehong <askarovbob.com>
2024-01-30 15:06:36 +0600

    Domashka

---
+++ homework/work8.py
@@ -0,0 +1,15 @@
+name = input("Kak zovut: ")
+film = input("Vash film: ")
+
+print("Privet, " + name)
+print("Horoshiy film " + film)
+
+a = 'Google создаст специальную команду для поиска багов в особо важных приложениях.'
+print(len(a.split()))
+
+a = 'Самые важные собЫтия в миРе инфосека за сентябрь'
+print(a.replace(" ", "|"))
+
+a = 'хакеры слили в сеть данные пакистанской энергетической компании k-Electric'
+print(a.title())
+
