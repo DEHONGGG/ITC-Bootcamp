76d024b86d17189c8c4ddb9ad43e857b54179264
Dehong <askarovbob.com>
2024-01-31 12:56:23 +0600

    Domashka

---
+++ homework/work12.py
@@ -0,0 +1,22 @@
+
+a = "segodnya pogoda norm"
+b = "hochetsya krichat"
+c = a.upper()+ " " + b.lower()
+print(c) 
+
+a = 1
+if a == 1:
+    print(a==1)
+
+a = "GitHub"
+b = input("simvol: ")
+print(a.split(b))
+
+a = "Github"
+b = input("Simvol: ")
+print(a.split(b))
+
+
+
+a = 'Ботнет IPStorm , в который ранее входили лишь Windows-машины, увеличился до 13 500 зараженных систем'""
+print(a.replace('е','3'))
